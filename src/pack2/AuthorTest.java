6b07d8551dc25581f012e2c65f524065e7c33585
OlegLesan <oleg.lesan1234@gmail.com>
2023-05-18 20:56:11 +0300

    3 commit

---
+++ src/pack2/AuthorTest.java
@@ -0,0 +1,8 @@
+package pack2;
+
+public class AuthorTest {
+    public static void main(String[] args) {
+        Person persoana1 = new Person ()
+        System.out.println(persoana1.toString());
+    }
+}
